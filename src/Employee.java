16d752a4bbd135adf7bc6264f536553b3aa30222
btm-fac <btm-fac@btm-fac6-PC>
2018-04-19 09:31:37 +0530

    cb

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Employee {
 	String name;
@@ -6,5 +7,12 @@ public class Employee {
 	String address;
 	double salary;
 	String empId;
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		Scanner s = new Scanner(System.in);
+		e.name = s.next();
+
+	}
 
 }
